9201c42a000cbe1325631c1a2de970eac0a870b9
hymanlew <wohena3689>
2019-12-14 00:04:27 +0800

    update redis

--- src/main/java/moreway/redis/RedisUtil.java
+++ src/main/java/moreway/redis/RedisUtil.java
@@ -48,16 +48,19 @@ public final class RedisUtil {
     private static JedisPool getPool() {
         if (pool == null) {
             JedisPoolConfig config = new JedisPoolConfig();
+
             //控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
             //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
             config.setMaxTotal(Integer.valueOf(Constant.getProperty("jedis.MaxTotal", "").trim()));
+
             //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
             config.setMaxIdle(Integer.valueOf(Constant.getProperty("jedis.MaxIdle", "").trim()));
+
             //表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
             config.setMaxWaitMillis(Integer.valueOf(Constant.getProperty("jedis.MaxWaitMillis", "").trim()));
+
             //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
             config.setTestOnBorrow(true);
-
 
             String redisIp = Constant.getProperty("jedis.host", "").trim();
             Integer redisPort = Integer.valueOf(Constant.getProperty("jedis.port", "").trim());
